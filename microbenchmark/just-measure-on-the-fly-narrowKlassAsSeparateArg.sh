2b01294613ff0b61b7e937e19440f0552955fe2c
tstuefe <thomas.stuefe@gmail.com>
2024-07-31 08:35:57 +0200

    scripts

---
+++ microbenchmark/just-measure-on-the-fly-narrowKlassAsSeparateArg.sh
@@ -0,0 +1,28 @@
+export ISOLATE_CPUS_COMMAND="chrt -r 1 taskset 0xFF00  "
+#xport ISOLATE_CPUS_COMMAND=" "
+export PERF_COMMAND=" perf stat  -B -e  L1-dcache-load-misses,L1-dcache-loads,LLC-load-misses,LLC-loads,dTLB-load-misses,dTLB-loads,instructions,branches "
+#export PERF_COMMAND=" "
+
+export JDK_A_NAME="jdk-v4"
+export JDK_B_NAME="jdk-v4-no-narrowKlass-argument"
+export JDK_C_NAME="gibsnich"
+
+
+export OPTIONS_A="-XX:+UnlockExperimentalVMOptions -XX:+UseCompactObjectHeaders -XX:+UseKLUT"
+export OPTIONS_B="-XX:+UnlockExperimentalVMOptions -XX:+UseCompactObjectHeaders -XX:+UseKLUT"
+export OPTIONS_C=" "
+
+#export TESTCLASSES_IN_BOOT_CLASSPATH="0"
+
+export USEGC="-XX:+UseParallelGC"
+#export USEGC="-XX:+UseG1GC"
+
+#export NUM_CLASSES=1024
+export NUM_CLASSES=1024
+export NUM_FULL_GC=10
+
+#export EXTRA_TEST_OPTIONS=" --wire-up"
+export EXTRA_TEST_OPTIONS=" "
+
+bash ./measure.sh
+
